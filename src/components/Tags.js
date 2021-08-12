57820b43d331a7856881a3ab1d3c1230a226a9bd
jaredwuu <jiachaowu@hotmail.com>
2021-08-11 22:43:46 -0400

    up

--- src/components/Tags.js
+++ src/components/Tags.js
@@ -1,11 +1,13 @@
 import React from 'react'
 
-const Tags = ({student }) => {
+const Tags = ({ student }) => {
     return (
         <div className='flex'>
             {student.tags.map((tag, i) => {
                 return (
+
                     <div key={i} className="bg-gray-700 mx-2 px-3 rounded">{tag}</div>
+
                 )
             })}
         </div>
--- test.js
+++
@@ -1,6 +0,0 @@
-const test = [];
-
-test[0] = [1,2,2];
-test[1]=[2,2,3];
-console.log(test[0]);
-
